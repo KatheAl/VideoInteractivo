b91bc0235655c4ba942733fee25cff2a4f6ff94b
Kathe <andreaalvaradocalle2717@gmail.com>
2025-12-18 12:46:39 -0500

    se mejoro el fondo

--- Sketch.js
+++ Sketch.js
@@ -9,10 +9,10 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(640, 480);
+  createCanvas(windowWidth, windowHeight);
 
   video = createCapture(VIDEO);
-  video.size(640, 480);
+  video.size(windowWidth, windowHeight);
   video.hide();
 
   handPose.detectStart(video, gotHands);
